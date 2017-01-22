a3581c68fb8e457976f6bd8caa9cdb6d3bc9d82b
palanglois <thefroggy27@gmail.com>
2017-01-22 18:02:48 +0100

    Using re instead of BeautifulSoup for performance

--- static/saveEdt.py
+++ static/saveEdt.py
@@ -1,18 +1,15 @@
 import urllib2
 import csv
 import os
-import BeautifulSoup as bs
+import re
 
 #Url of the page where the edt is located
 edtUrl = "http://www.math.ens-cachan.fr/planning--368972.kjsp?RH=1242415112528"
 pageEdt = urllib2.urlopen(edtUrl).read()
-soup = bs.BeautifulSoup(pageEdt)
-#Finding the "<li class="xls">" tag
-linkLocation = soup.findAll('li',{ "class" : "xls"})[0]
-#Extracting the first hypertext link inside of it
-links = bs.BeautifulSoup(str(linkLocation),parseOnlyThese=bs.SoupStrainer('a'))
-linkTab = [link for link in links]
-link=str(linkTab[0]['href'])
+a = [m.start() for m in re.finditer(r'xls',pageEdt)][0]
+pageRest = pageEdt[a+14:-1]
+b = [m.start() for m in re.finditer(r'onclick',pageRest)][0]
+link = pageRest[0:b-2].replace("&amp;","&")
 
 #Download and save the excel file to the static directory
 def getAndParseEdt(url):
