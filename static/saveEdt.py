8e561cd22108ef85847d458cc555c25eb6a801c7
palanglois <thefroggy27@gmail.com>
2017-01-22 17:32:35 +0100

    Modyfing the function getting the xls file because its adress is updated when the file changes. Now we get it from the html page.

--- static/saveEdt.py
+++ static/saveEdt.py
@@ -1,8 +1,18 @@
 import urllib2
 import csv
 import os
+import BeautifulSoup as bs
 
-edtUrl = "http://www.math.ens-cachan.fr/servlet/com.univ.collaboratif.utils.LectureFichiergw?ID_FICHE=19520&OBJET=0017&ID_FICHIER=669305"
+#Url of the page where the edt is located
+edtUrl = "http://www.math.ens-cachan.fr/planning--368972.kjsp?RH=1242415112528"
+pageEdt = urllib2.urlopen(edtUrl).read()
+soup = bs.BeautifulSoup(pageEdt)
+#Finding the "<li class="xls">" tag
+linkLocation = soup.findAll('li',{ "class" : "xls"})[0]
+#Extracting the first hypertext link inside of it
+links = bs.BeautifulSoup(str(linkLocation),parseOnlyThese=bs.SoupStrainer('a'))
+linkTab = [link for link in links]
+link=str(linkTab[0]['href'])
 
 #Download and save the excel file to the static directory
 def getAndParseEdt(url):
@@ -10,5 +20,5 @@ def getAndParseEdt(url):
   f = open("/var/www/myServ/FlaskApp/static/edt.xls",'w')
   f.write(content)
 
-getAndParseEdt(edtUrl)
+getAndParseEdt(link)
 
